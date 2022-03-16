0fe17143fe05c2230fe973388fedf9bd4839452b
Anna <sharipkinanna@gmail.com>
2022-03-16 15:40:53 +0200

    projectSetup,addingAudience with laravelP ermission

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+Route::group(['middleware' => ['role:admin']], function () {
+    //
+});
